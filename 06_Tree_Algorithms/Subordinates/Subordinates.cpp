5602ffb69e2c584bb86378574b8fa9c8c09aaaf4
axrdiv-PC <axrdiv@outlook.com>
2020-11-07 21:42:51 +0800

    A bit of `Tree Algorithms` Problems

--- 06_Tree_Algorithms/Subordinates/Subordinates.cpp
+++ 06_Tree_Algorithms/Subordinates/Subordinates.cpp
@@ -10,9 +10,8 @@ int main() {
     for(int i = 0; i < n-1; i++)
         cin >> root[i];
 
-    for(int i = n-2; i >= 0; i--) {
+    for(int i = n-2; i >= 0; i--)
         sub[root[i]] += sub[i+2] + 1;
-    }
 
     for(int i = 1; i <= n; i++)
         cout << sub[i] << (i == n ? '\n' : ' ');
